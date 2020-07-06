49c89862adc541372108db7e5d0863844437a3b4
Tosca van Schaik <toscavanschaik@gmail.com>
2020-07-06 13:12:21 +0200

    added solutions

--- exercises/arrays-01.js
+++ exercises/arrays-01.js
@@ -14,7 +14,7 @@ const rows = Array.from(new Array(numberOfRows)).map((_) =>
   )
 );
 const table = [headerRow, ...rows];
-console.table(table);
+//console.table(table);
 
 /* EXERCISE 1 
 Use console.log(table) and console.table(table) to examine `table`.
@@ -34,14 +34,52 @@ Use console.log to show the result.
 
 You may only use for loops, and no array methods.
 */
-console.log();
+
+function convertTable(array) {
+  const output = [];
+  const departments = array[0];
+  const employeeRows = array.slice(1);
+  for (
+    let employeeRowIndex = 0;
+    employeeRowIndex < employeeRows.length;
+    employeeRowIndex++
+  ) {
+    const rowObject = {};
+    const employeeRow = employeeRows[employeeRowIndex];
+    for (
+      let employeeIndex = 0;
+      employeeIndex < employeeRow.length;
+      employeeIndex++
+    ) {
+      rowObject[departments[employeeIndex]] = employeeRow[employeeIndex];
+    }
+    output.push(rowObject);
+  }
+  return output;
+}
+
+console.log(convertTable(table));
 
 /* EXERCISE 2
 Get the same result as EXERCISE 1, but now with only chained array 
 methods and no for loops.
 */
 
-console.log();
+const convertedTable = table.slice(1).reduce((output, employeeRow) => {
+  const departments = table[0];
+  const newObject = {};
+  for (
+    let employeeIndex = 0;
+    employeeIndex < employeeRow.length;
+    employeeIndex++
+  ) {
+    newObject[departments[employeeIndex]] = employeeRow[employeeIndex];
+  }
+  output.push(newObject);
+  return output;
+}, []);
+
+console.log(convertedTable);
 
 /* EXERCISE 3 
 Convert `table` to a single object that has a key for each department 
@@ -49,4 +87,42 @@ and arrays of people in those departments as values.
 
 You can use for loops or array methods.
 */
-console.log();
+
+function departmentColumns(array) {
+  const departments = array[0];
+  const employees = array.slice(1);
+  const sortedByColumn = [];
+  for (department of departments) {
+    sortedByColumn.push([department]);
+  }
+  for (
+    let employeeRowIndex = 0;
+    employeeRowIndex < employees.length;
+    employeeRowIndex++
+  ) {
+    const currentRow = employees[employeeRowIndex];
+    for (
+      let employeeIndex = 0;
+      employeeIndex < departments.length;
+      employeeIndex++
+    ) {
+      const currentEmployee = currentRow[employeeIndex];
+      sortedByColumn[employeeIndex].push(currentEmployee);
+    }
+  }
+  return sortedByColumn;
+}
+
+const employeesByDepartment = departmentColumns(table).reduce(
+  (output, department) => {
+    const departmentKey = department[0];
+    const departmentEmployees = department.slice(1);
+    if (!output[departmentKey]) {
+      output[departmentKey] = departmentEmployees;
+    }
+    return output;
+  },
+  {}
+);
+
+console.log(employeesByDepartment);
--- exercises/arrays-03.js
+++ exercises/arrays-03.js
@@ -12,22 +12,73 @@ for (let i = 0; i < 10; i++) {
 }
 
 for (const user of twitterUsers) {
-  const followable = twitterUsers.filter((p) => p.name !== user.name);
+  const followableUsers = twitterUsers.filter(
+    (followableUser) => followableUser !== user
+  );
   for (let i = 0; i < faker.random.number(5); i++) {
-    const randomUser = followable.splice(
-      followable.indexOf(faker.random.arrayElement(followable)),
+    const randomUser = followableUsers.splice(
+      followableUsers.indexOf(faker.random.arrayElement(followableUsers)),
       1
     )[0];
-
     user.following.push(randomUser);
   }
 }
 
-console.log(twitterUsers);
-
 /* EXERCISE 1
-Give each twitter user a property 'followers' with an array 
+1. Give each twitter user a property 'followedBy' with an array 
 of all their followers. 
 
-Sort the array of users by most followed. 
+2. Sort the array of users by most followed. 
  */
+
+for (const user of twitterUsers) {
+  user.followedBy = [];
+  for (const possibleFollower of twitterUsers) {
+    for (const followedUser of possibleFollower.following) {
+      if (followedUser === user) {
+        user.followedBy.push(possibleFollower);
+        break;
+      }
+    }
+  }
+}
+twitterUsers.sort((a, b) => {
+  return a.followedBy.length - b.followedBy.length;
+});
+
+/* EXERCISE 2
+Give each twitter user a property 'bestFriend'.
+Choose a random twitter user as the value.
+- You must use faker.random.arrayElement(arr) to select the random best friends
+- One user can only be best friends with ONE other user
+ */
+
+/*
+const teamA = twitterUsers.slice(Math.floor(twitterUsers.length / 2));
+const teamB = twitterUsers.slice(0, Math.floor(twitterUsers.length / 2));
+
+for (const i of teamA) {
+  const newBestFriend = faker.random.arrayElement(teamB);
+  i.bestFriend = newBestFriend;
+  newBestFriend.bestFriend = i;
+  teamB.splice(teamB.indexOf(newBestFriend), 1);
+}
+*/
+
+let possibleBestFriends = twitterUsers.slice();
+
+for (const user of possibleBestFriends) {
+  possibleBestFriends = possibleBestFriends.filter(
+    (friend) => friend !== user && !friend.bestFriend
+  );
+  if (possibleBestFriends.length === 0) {
+    break;
+  }
+  if (user.bestFriend) {
+    continue;
+  }
+  user.bestFriend = faker.random.arrayElement(possibleBestFriends);
+  user.bestFriend.bestFriend = user;
+}
+
+console.log();
